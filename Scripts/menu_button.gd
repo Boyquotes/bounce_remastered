0e559c6016ce5d617cda31c82e7e561ed31c80fb
Troy Amadeus <troy.amadeus@ui.ac.id>
2019-11-04 09:56:57 +0700

    [CHORES] Recycle some generic scenes and its depedency from 5th week tutorial.

---
+++ Scripts/menu_button.gd
@@ -0,0 +1,11 @@
+extends LinkButton
+
+func _option_new_game():
+	get_tree().change_scene(str("res://Scenes/Maps/map_classic_lv1.tscn"))
+
+func _option_quit():
+	get_tree().quit()
+
+func _option_return_to_main_menu():
+	get_tree().change_scene(str("res://Scenes/Generic/main_menu.tscn"))
+	global.lives = 3
